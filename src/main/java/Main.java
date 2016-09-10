333b0bdf311415ce92420cffd51ca711f79366a1
Jun Nguyen <n_jun01@hotmail.com>
2016-09-10 16:18:59 -0400

    pom setted up

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jun on 9/10/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/Utility.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jun on 9/10/2016.
+ */
+public class Utility {
+    public void xmlParser(){
+
+
+    }
+}
