de273e72e49b739a72cd29f57605e4db92588940
Jun Nguyen <n_jun01@hotmail.com>
2016-09-14 11:23:02 -0400

    utility implemented

--- src/main/java/decisionTree/Utility.java
+++ src/main/java/decisionTree/Utility.java
@@ -1,9 +1,13 @@
 package decisionTree;
 
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Utility {
@@ -13,20 +17,28 @@ public class Utility {
         try {
             while ((input = inputReader.readLine()) != null) {
                 System.out.println(input);
-                String[] split = input.split("\\s+");
+                String[] split = input.split("\\s+",4);
                 if (Arrays.asList(split).contains("<node")) {
                     String behavior = "";
                     String response = "";
+                    Map map = new HashMap();
                     for (int i = 0; i < split.length; i++) {
-                        if (split[i].contains("behavior")) {
-                            String[] attribute = split[i].split("\"");
-                            behavior = attribute[1];
-                        } else if (split[i].contains("response")) {
-                            String[] attribute = split[i].split("\"");
-                            response = attribute[1];
+//                        if (split[i].contains("behavior")) {
+//                            if (!split[i].contains("\"\"")) {
+//                                String[] attribute = split[i].split("\"");
+//                                behavior = attribute[1];
+//                            }
+//                        } else if (split[i].contains("response")) {
+//                            if (!split[i].contains("\"\"")) {
+//                                String[] attribute = split[i].split("\"");
+//                                behavior = attribute[1];
+//                            }
+//                        }
+                        if(split[i].contains("=")){
+//                            map.put.readXml(split[i]);
                         }
                     }
-                    new Node(behavior,response);
+                    new Node(map.get("behavior").toString(), map.get("response").toString());
                 }
             }
         } catch (IOException exception) {
@@ -34,7 +46,10 @@ public class Utility {
         }
     }
 
-    static void parse(String line) {
-
+    static Map readXml(String line) {
+        String[] split = line.split("=");
+        Map result = new HashMap<String,String>();
+        result.put(split[0],split[1].split("\"")[1]);
+        return result;
     }
 }
